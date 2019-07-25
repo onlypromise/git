36099a83811a3ce061079aa2c1f10e59d0aff339
chenlei <893493381@qq.com>
2019-07-25 20:01:00 +0800

    7.25日修改

--- maven_web/src/main/java/a/demo1.java
+++ maven_web/src/main/java/a/demo1.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 public class demo1 extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request,response);
+		System.out.println("Äã»¹°®ÎÒÂð£¿");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
